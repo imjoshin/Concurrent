9a43c4349291b0f3ae897caf7b965295d55b2653
j0sh77 <jocjohns@mtu.edu>
2015-04-25 14:08:24 -0400

    Added questions/slides for weeks 10-13

--- questions.php
+++ questions.php
@@ -11,15 +11,18 @@
 		$HTML .= "<h1 data-num='" . ($wNum + 1) . "'>Week " . ($wNum + 1) . " <i class='fa fa-minus-square-o'></i></h1>";
 		$HTML .= "<div class='week small-12 columns' data-num='" . ($wNum + 1) . "'>";
 
+		$qPrefix = 1;
 		foreach(array_slice($questions, 1) as $qNum=>$q){
 			if(file_exists("slides/" . trim($q))){
 				$HTML .= "<a class='slides button secondary small' href='slides/$q' target='_blank'>$q</a><br>";
 			}else{
 				$answers = explode("<a>", $q);
 
-				$HTML .= "<div class='question'><b>$answers[0]</b></div>";
+				$HTML .= "<div class='question'><b>$qPrefix. $answers[0]</b></div>";
 				$HTML .= "<div class='answer'>$answers[1]</div>";
 				$HTML .= "<br>";
+
+				$qPrefix++;
 			}
 		}
 
